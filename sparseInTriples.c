a49f0bcf36eb6f656481b6b9d252163358da8f52
Aditya Panwar <aditya.panwar.2208@gmail.com>
2022-09-25 14:25:21 +0530

    input

--- sparseInTriples.c
+++ sparseInTriples.c
@@ -2,6 +2,19 @@
 
 int main()
 {
+    int row, column;
+    printf("Enter the size of matrix (rows, columns): ");
+    scanf("%d%d", &row, &column);
+    int array[row][column];
+    printf("Enter elements of matrix\n");
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            printf("[%d][%d]:", i, j);
+            scanf("%d", &array[row][column]);
+        }
+    }
 
     return 0;
 }
