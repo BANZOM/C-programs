cf3cbb9abc90cf2c448ec1b7ed5ba89627ad41a7
Aditya Panwar <aditya.panwar.2208@gmail.com>
2022-09-25 15:14:43 +0530

    working good, if not commited again then its final

--- sparseInTriples.c
+++ sparseInTriples.c
@@ -9,13 +9,13 @@ int noOfNonZeros(int rows, int cols, int array[rows][cols])
     {
         for (int j = 0; j < cols; j++)
         {
-            printf("%d,  ", array[i][j]);
+            // printf("%d,  ", array[i][j]);
             if (array[i][j] > 0 || array[i][j] < 0)
             {
                 nonZeros++;
             }
         }
-        printf("\n");
+        // printf("\n");
     }
     return nonZeros;
 }
@@ -41,7 +41,31 @@ int main()
     // count of non zero numbers
     count = noOfNonZeros(row, column, array);
 
-    printf("**%d**", count);
+    // printf("**%d**", count); // to check if the count works perfectly
+
+    int whichRow[count], whichColumn[count], whichNum[count];
+
+    int k = 0;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            if (array[i][j] > 0 || array[i][j] < 0)
+            {
+                whichRow[k] = i;
+                whichColumn[k] = j;
+                whichNum[k] = array[i][j];
+                k++;
+            }
+        }
+    }
+
+    // printing into triples
+    printf("%d\t%d\t%d\n", row, column, count);
+    for (int i = 0; i < count; i++)
+    {
+        printf("%d\t%d\t%d\n", whichRow[i], whichColumn[i], whichNum[i]);
+    }
 
     return 0;
 }
