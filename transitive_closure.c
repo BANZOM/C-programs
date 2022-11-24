de7bea62584381d1a29f3db68ffb2a9f8aa11dc5
BANZOM <aditya.panwar.2208@gmail.com>
2022-11-24 20:05:58 +0530

    working on transitive closure

--- transitive_closure.c
+++ transitive_closure.c
@@ -6,6 +6,7 @@ int main(int argc, char const *argv[])
     // input the no. of nodes/ row size
     scanf("%d", &n);
 
+    // input the array matrix of n*n
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
@@ -13,6 +14,16 @@ int main(int argc, char const *argv[])
             scanf("%d", &a[i][j]);
         }
     }
+
+    // logic of the transitive closure
+    for (int iteration = 0; iteration < n; iteration++)
+    {
+        
+        
+    }
+    
+    
+
 
     return 0;
 }
