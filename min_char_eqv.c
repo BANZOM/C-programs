7596f9653eaac6f79ee4f0caa1a4818cab31568d
BANZOM <aditya.panwar.2208@gmail.com>
2022-12-14 21:34:49 +0530

    working on smallestNum function

--- min_char_eqv.c
+++ min_char_eqv.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 
-long int smallestNum( char *str)
+long int smallestNum(char *str)
 {
-    return 0;
+    int nums[100] = {0}, n=0,i=0;
+    while (*str != '\0')
+    {
+        char c = *str;
+        printf("%c\n", c);
+
+        
+        str++;
+    }
+
+    // return 0;
 }
 int main()
 {
-    char *string,str[100];
+    char *string, str[100];
     string = str;
     printf("Enter the string: ");
-    scanf(" %s",string);
+    scanf(" %s", string);
 
     // printf("%s\n",str);   // prints the input string
 
-    printf("Smallest number = %ld\n",smallestNum(str));
+    printf("Smallest number = %ld\n", smallestNum(str));
 
     return 0;
 }
