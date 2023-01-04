89b349367e7cc44917e53184b80eca44f6cdc2b7
BANZOM <aditya.panwar.2208@gmail.com>
2023-01-04 23:30:26 +0530

    scan and print

--- BST_inorder_print_ByPos.c
+++ BST_inorder_print_ByPos.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+void print(char *a, int n)
+{
+    for (int i = 0; i < n; i++)
+        printf("%c ", *(a+i));
+}
 int main()
 {
     int n;
@@ -7,6 +12,12 @@ int main()
     printf("Enter the number of nodes: ");
     scanf("%d", &n);
     tree = calloc(n, sizeof(char));
+    for (int i = 0; i < n; i++)
+    {
+        scanf(" %c", tree + i);
+    }
 
+    print(tree, n);
+    printf("\n");
     return 0;
 }
