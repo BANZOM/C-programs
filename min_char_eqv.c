cdebc9dac19c32fdcdcfa812d5226e938639cf18
BANZOM <aditya.panwar.2208@gmail.com>
2023-01-04 23:08:23 +0530

    not completed

--- min_char_eqv.c
+++ min_char_eqv.c
@@ -1,26 +1,49 @@
 #include <stdio.h>
 
+int maxOfArr(int nums[])
+{
+    int i = 0, max = nums[0];
+    while (nums[i] != 0)
+    {
+        if (max > nums[i])
+        {
+            max = nums[i];
+        }
+        i++;
+    }
+    return max;
+}
 long int smallestNum(char *str)
 {
-    int nums[100] = {0}, n=0,i=0,count = 0;
+    int nums[100] = {0}, n = 0, i = 0, count = 1;
     while (*str != '\0')
     {
-        // printf("%c\n", c);
-        count = 0;
-        while (*str != 'I' && *str != '\0')
+        count = 1;
+        while (*str == 'D' || *str != 'I' && *str != '\0')
         {
-            // char c = *str;
-            // printf("%c\n", c);
-
             count++;
             str++;
         }
-        
-        printf("%d*\n",count);
-        
+        printf("%d*\n", count);
+        while (count != 0)
+        {
+            int temp = maxOfArr(nums) + count;
+            nums[i++] = temp--;
+            count--;
+        }
+
         str++;
     }
 
+    for (int i = 0; i < 100; i++)
+    {
+        if (nums[i] == 0)
+        {
+            break;
+        }
+        printf("%d", nums[i]);
+    }
+    printf("\n");
     // return 0;
 }
 int main()
