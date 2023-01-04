be9c37ab32cdc8204097f8dc01bdfd5dcc55ef12
BANZOM <aditya.panwar.2208@gmail.com>
2023-01-04 23:47:48 +0530

    modified: char to int

--- BST_inorder_print_ByPos.c
+++ BST_inorder_print_ByPos.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-void print(char *a, int n)
+void print(int *a, int n)
 {
     for (int i = 0; i < n; i++)
-        printf("%c ", *(a+i));
+        printf("%d ", *(a + i));
 }
 int main()
 {
     int n;
-    char *tree;
+    int *tree, *inorder;
     printf("Enter the number of nodes: ");
     scanf("%d", &n);
-    tree = (char *)malloc(n*sizeof(char));
 
+    // memory allocation
+    tree = malloc(n * sizeof(int));
+    inorder = calloc(n, sizeof(int));
+    
+    printf("Enter the data in BST order: ");
+    // read
     for (int i = 0; i < n; i++)
     {
-        scanf(" %c", tree + i);
+        scanf(" %d", tree + i);
     }
 
     print(tree, n);
+
     printf("\n");
     return 0;
 }
