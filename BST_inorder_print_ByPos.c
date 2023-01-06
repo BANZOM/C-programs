fc7da9ae91d4bea9fef10077476ff42bd07c65fc
BANZOM <aditya.panwar.2208@gmail.com>
2023-01-06 10:57:59 +0530

    Remove Extras

--- BST_inorder_print_ByPos.c
+++ BST_inorder_print_ByPos.c
@@ -3,19 +3,11 @@
 
 int c = 0; // call by reference is not working so declaring global variable
 
-void print(int *a, int n)
-{
-    for (int i = 0; i < n; i++)
-        printf("%d ", a[i]);
-}
-
 // inoder traversal in arrays
 void iOT(int *a, int indx, int n, int *inorder)
 {
     if (*(a + indx) == -99 || indx >= n)
-    {
         return;
-    }
     else
     {
         iOT(a, 2 * indx + 1, n, inorder);
@@ -26,9 +18,7 @@ void iOT(int *a, int indx, int n, int *inorder)
 
 int main()
 {
-    int n;  // num of nodes
-    int pos; // which pos to print of inorder
-    int *tree, *inorder;
+    int n, pos, *tree, *inorder;
 
     printf("Enter the number of nodes: ");
     scanf("%d", &n);
@@ -40,18 +30,14 @@ int main()
     // input the tree
     printf("Enter the data in BST order (-99 for NULL): ");
     for (int i = 0; i < n; i++)
-    {
         scanf("%d", &tree[i]); // -99 for NULL
-    }
 
     // inorder traversal and store into array 'inorder'
     iOT(tree, 0, n, inorder);
 
-    // print(inorder, c); // for troubleshoot
-
     printf("Enter the position to print: ");
     scanf("%d", &pos);
-    printf("%d\n", inorder[pos - 1]);
+    printf("Answer = %d\n", inorder[pos - 1]);
 
     free(tree);
     free(inorder);
