51b799b6a59b499b24ee53e7d62cbaecd43e3e24
BANZOM <aditya.panwar.2208@gmail.com>
2023-01-10 19:10:56 +0530

    scan

--- print_leaf_ofTREE.c
+++ print_leaf_ofTREE.c
@@ -3,9 +3,13 @@
 int main()
 {
     int tree[1000],n;
-    printf("Enter number of data to be store: ");
+    printf("TREE IS IN ARRAY REPRESENTATION\nEnter number of data to be store: ");
     scanf("%d",&n);
     printf("Enter the elements of tree (-9 for NULL): ");
+    
+    for (int i = 0; i < n; i++)
+        scanf("%d",&tree[i]);
+    
 
     return 0;
 }
