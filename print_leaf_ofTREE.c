99418552e20ea48c4f00dd5c78966d5c91ec42c2
BANZOM <aditya.panwar.2208@gmail.com>
2023-01-10 19:08:48 +0530

    Read Elements

--- print_leaf_ofTREE.c
+++ print_leaf_ofTREE.c
@@ -2,8 +2,9 @@
 
 int main()
 {
-    int tree[1000];
-    
+    int tree[1000],n;
+    printf("Enter number of data to be store: ");
+    scanf("%d",&n);
     printf("Enter the elements of tree (-9 for NULL): ");
 
     return 0;
