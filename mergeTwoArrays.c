6c452389c3d5afa383b3aa502e86c87ee4f6366e
Aditya Panwar <93468551+BANZOM@users.noreply.github.com>
2023-01-20 18:37:14 +0530

    Rewrite the program

--- mergeTwoArrays.c
+++ mergeTwoArrays.c
@@ -1,25 +1,24 @@
 // Given two sorted arrays of size M and of size N. Merge the two arrays into one array (Sorted) without using any extra array.
 
-#include<stdio.h>
+#include <stdio.h>
 
-void mergeArray(int a[], int b[], int m, int n){
-    
-}
+int main() {
+  int m, n, a[100], b[100];
+  scanf("%d%d", &m, &n);
 
-void main(){
-    int m,n;
-    scanf("%d%d",&m,&n);
-    
-    int arr1[100],arr2[100];
-    
-    for(int i=0; i<m; i++)
-        scanf("%d",&arr1[i]);
-    
-    for(int i=0; i<n; i++)
-        scanf("%d",&arr2[i]);
-        
-    mergeArray(arr1,arr2,m,n);
-    
-    for(int i=0; i<m+n; i++)
-        printf("%d ",arr1[i]);
+  for (int i = 0; i < m; i++) {
+    scanf("%d", &a[i]);
+  }
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &b[i]);
+  }
+
+  // merge
+  merge(a, m, b, n);
+
+  for (int i = 0; i < m + n; i++) {
+    printf("%d ", a[i]);
+  }
+  printf("\n");
+  return 0;
 }
