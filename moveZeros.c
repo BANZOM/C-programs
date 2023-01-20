51ed65520888b130c38de069a9b3d46b78a48def
Aditya Panwar <93468551+BANZOM@users.noreply.github.com>
2023-01-20 17:37:09 +0530

    Update moveZeros.c

--- moveZeros.c
+++ moveZeros.c
@@ -3,3 +3,25 @@
 // Before: 0 2 0 2 0 
 // After: 2 2 0 0 0 
 // Move zeros to back
+
+#include <stdio.h>
+
+int main() {
+  int n;
+  scanf("%d", &n);
+  int arr[n];
+  for (int i = 0; i < n; i++)
+    scanf("%d", &arr[i]);
+
+  printf("Before: ");
+  print(arr, n);
+
+  // logic to move 0 to end;
+  moveZeros(arr, n);
+
+  printf("After: ");
+  print(arr, n);
+
+  // free(arr);
+  return 0;
+}
