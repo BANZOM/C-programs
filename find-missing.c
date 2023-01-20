0715c9993398637c063ef62bd0a998a6a8fad522
Aditya Panwar <93468551+BANZOM@users.noreply.github.com>
2023-01-20 19:11:58 +0530

    Added info about the program

--- find-missing.c
+++ find-missing.c
@@ -1,1 +1,2 @@
-
+// Consider an array (assume unsorted) having positive numbers. Your task is to find out the smallest missing number from the given array.
+// If the array has no such elements, then print the next element in sorted order.
