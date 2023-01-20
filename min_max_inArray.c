35cd6fc33f483057f4b4facf66304aa71d4d83fe
Aditya Panwar <93468551+BANZOM@users.noreply.github.com>
2023-01-20 16:47:29 +0530

    Update min_max_inArray.c

--- min_max_inArray.c
+++ min_max_inArray.c
@@ -1,1 +1,18 @@
 // minimum and maximum in an 1D-array
+// we'll give argument of variables to store min and max value in main function;
+
+#include<stdio.h>
+
+void main()
+{
+    int n; 
+    scanf("%d",&n);
+    int arr[n];
+    
+    for(int i=0; i<n; i++)
+        scanf("%d",&arr[i]);
+    
+    int min,max;
+    returnMinMax(arr,&min,&max,n);
+    printf("%d\n%d",min,max);
+}
