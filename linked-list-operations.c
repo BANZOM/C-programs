2a07550772c87faadfd67cb07c9be1d3f88ea8c9
Aditya Panwar <93468551+BANZOM@users.noreply.github.com>
2023-01-21 12:09:09 +0530

    Update linked-list-operations.c

--- linked-list-operations.c
+++ linked-list-operations.c
@@ -27,4 +27,36 @@ int main(void) {
   printf("Functions:\n1. Insertion from beginning\n2. Deletion from Beginning\n3. Traversing the list\n4. Finding length of the list\n5. Insert at last\n6. Delete from last\n7. Insert after position\n8. Delete at position\n0. Exit\n");
   printf("Choose and input any one option: ");
   scanf("%d",&choice);
+  
+  switch (choice) {
+    case 0:
+      printf("Quitting\n");
+      exit(0);
+    case 1:
+      insertAtbeg();
+      break;
+    case 2:
+      deleteFromBeg();
+      break;
+    case 3:
+      traverse();
+      break;
+    case 4:
+      printf("Current length= %d\n", findLen());
+      break;
+    case 5:
+      insertAtLast();
+      break;
+    case 6:
+      deleteFromLast();
+      break;
+    case 7:
+      insertAfterPosition();
+      break;
+    case 8:
+      deleteAtPosition();
+      break;
+    default:
+      printf("Wrong Input, Enter 0 to exit.\n\n");
+  }
 }
