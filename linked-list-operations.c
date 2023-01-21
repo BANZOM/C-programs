14d9f8b69c8ede1d6c31e12be82f606faf3f8a59
Aditya Panwar <93468551+BANZOM@users.noreply.github.com>
2023-01-21 11:36:18 +0530

    Update linked-list-operations.c

--- linked-list-operations.c
+++ linked-list-operations.c
@@ -2,6 +2,10 @@
 1. Insertion from beginning
 2. Deletion from Beginning
 3. Traversing the list
-4. Fingind length of the list
+4. Finding length of the list
 5. Insert at last
 6. Delete from last
+7. Insert after position
+8. Delete at position
+
+
