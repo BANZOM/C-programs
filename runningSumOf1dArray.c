8120e6377d5b9faff8adb6b2dd0264d139b8fb88
Aditya Panwar <93468551+BANZOM@users.noreply.github.com>
2023-01-27 17:39:30 +0530

    Create runningSumOf1dArray.c

---
+++ runningSumOf1dArray.c
@@ -0,0 +1,5 @@
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+// Return the running sum of nums.
+// Input: nums = [1,1,1,1,1]
+// Output: [1,2,3,4,5]
+// Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
