ae586b2a0c82693fb2a651501f305f6e8fa6bbd4
Aditya Panwar <93468551+BANZOM@users.noreply.github.com>
2023-01-27 17:40:51 +0530

    Update runningSumOf1dArray.c

--- runningSumOf1dArray.c
+++ runningSumOf1dArray.c
@@ -3,3 +3,19 @@
 // Input: nums = [1,1,1,1,1]
 // Output: [1,2,3,4,5]
 // Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* runningSum(int* nums, int numsSize, int* returnSize){
+    int *ans = (int*)malloc(numsSize*sizeof(int));
+    int sum=0;
+    for(int i=0; i<numsSize; i++){
+        sum += nums[i];
+        ans[i]=sum;
+    }
+    *returnSize = numsSize;
+    return ans;
+}
+
+// This is a function that return the output
