82460431176599709e429fd00066116ca92eba7f
Aditya Panwar <93468551+BANZOM@users.noreply.github.com>
2023-01-29 21:07:49 +0530

    Update is-subsequence.c

--- is-subsequence.c
+++ is-subsequence.c
@@ -5,6 +5,12 @@ A subsequence of a string is a new string that is formed from the original strin
 deleting some (can be none) of the characters without disturbing the relative positions of 
 the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not). 
 
+Example 1:
+Input: s = "abc", t = "ahbgdc"
+Output: true
 
+Example 2:
+Input: s = "axc", t = "ahbgdc"
+Output: false
 
 */
