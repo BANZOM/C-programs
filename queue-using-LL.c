43f8f4fdf59be0cba2c9c24195f6d9b89f6a42dd
BANZOM <aditya.panwar.2208@gmail.com>
2023-02-10 21:30:52 +0530

    initialization

---
+++ queue-using-LL.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void enqueue() {}
+void dequeue() {}
+void traverse() {}
+
+int main()
+{
+    int c;
+    printf("\n1. Enqueue\n2. Dequeue\n3. Traverse\n4. Exit\n");
+    printf("Enter your choice: ");
+    scanf("%d", &c);
+    switch (c) {
+        case 1: 
+            enqueue();
+            break;
+        case 2:
+            dequeue();
+            break;
+        case 3:
+            traverse();
+            break;
+        case 4:
+            exit(0);
+            break;
+        default:
+            printf("Invalid choice\n");
+    }
+    return 0;
+}
